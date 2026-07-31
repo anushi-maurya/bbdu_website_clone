2524a52fc1fc871e8a1650e104f8fd9366daa7a2
anushi-maurya <mauryaanushi@gmail.com>
2026-08-01 00:26:10 +0530

    Update database connection

--- src/main/java/util/DBConnection.java
+++ src/main/java/util/DBConnection.java
@@ -5,20 +5,22 @@ import java.sql.DriverManager;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/bbdu_db";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Nancy20!";
+    private static final String URL =
+            System.getenv().getOrDefault("DB_URL", "jdbc:mysql://localhost:3306/bbdu_db");
+
+    private static final String USER =
+            System.getenv().getOrDefault("DB_USER", "root");
+
+    private static final String PASSWORD =
+            System.getenv().getOrDefault("DB_PASSWORD", "");
 
     public static Connection getConnection() {
 
         Connection con = null;
 
         try {
-
             Class.forName("com.mysql.cj.jdbc.Driver");
-
             con = DriverManager.getConnection(URL, USER, PASSWORD);
-
         } catch (Exception e) {
             e.printStackTrace();
         }
